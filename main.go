5abb8bc29c37f183b788d90b6aabc5e75dc5f520
lyl <liuyulong@defensedriver.com.cn>
2024-06-25 11:51:46 +0800

    init

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/dapr-platform/common"
+	daprd "github.com/dapr/go-sdk/service/http"
+	"github.com/go-chi/chi/v5"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	httpSwagger "github.com/swaggo/http-swagger"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+var (
+	PORT = 80
+)
+
+func init() {
+
+	if val := os.Getenv("LISTEN_PORT"); val != "" {
+		PORT, _ = strconv.Atoi(val)
+	}
+	common.Logger.Debug("use PORT ", PORT)
+}
+
+// @title template-project API
+// @version 1.0
+// @description template-project API
+// @BasePath /swagger/template-project
+func main() {
+	mux := chi.NewRouter()
+	mux.Handle("/metrics", promhttp.Handler())
+	mux.Handle("/swagger*", httpSwagger.WrapHandler)
+	s := daprd.NewServiceWithMux(":"+strconv.Itoa(PORT), mux)
+	common.Logger.Debug("server start")
+	if err := s.Start(); err != nil && err != http.ErrServerClosed {
+		common.Logger.Fatalf("error: %v", err)
+	}
+}
